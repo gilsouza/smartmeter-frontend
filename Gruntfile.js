71f7d01db1c90942d614e51badae425e9782fd48
gilsouza <gilmarcs@gmail.com>
2015-06-14 21:51:08 -0300

    primeiros testes de layout. Ainda estatico. =)

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -11,13 +12,43 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+    clean: ["path/to/dir/one", "path/to/dir/two"],
+    browserSync: {
+        default_options: {
+            bsFiles: {
+                src: [
+                    "**/*.*"
+                ]
+            },
+            options: {
+              server: {
+                baseDir: "public"
+              }
+            }
+        }
+    },
+    concat: {
+        options: {
+            separator: ';',
+            stripBanners: true,
+            banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */',
+            },
+        dist: {
+            src: ['src/intro.js', 'src/project.js', 'src/outro.js'],
+            dest: 'dist/built.js',
+        },
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks("grunt-browser-sync");
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['browserSync']);
+  grunt.registerTask('build', ['clean', 'concat', 'uglify']);
 
 };
